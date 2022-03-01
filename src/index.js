dd052943ea4b1e09173d1b3ede5762e837e6fb5f
daudi13 <davouma13@gmail.com>
2022-03-01 22:01:59 +0300

    Add modules

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-	element.classList.add('hello');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
-	entry: './src/index.js',
-	mode: 'development',
-	devServer: {
-		static: './dist',
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    static: './dist',
   },
-	plugins: [
+  plugins: [
     new HtmlWebpackPlugin({
-			template: '/src/index.html'
+      template: '/src/index.html',
     }),
   ],
   output: {
     filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-		clean: true,
-	},
-	module: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-			},
-			{
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-          plugins: ['@babel/plugin-proposal-object-rest-spread']
-        }
-      }
-    }
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          },
+        },
+      },
     ],
   },
 };
