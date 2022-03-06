406648fb1136c13527716295b7a0db1229b985e5
daudi13 <davouma13@gmail.com>
2022-03-06 08:37:16 +0300

    Add notification opacity

--- src/modules/formdata.js
+++ src/modules/formdata.js
@@ -15,8 +15,20 @@ export const addScore = (url, id, score, bodyObject, hol) => {
     body: JSON.stringify(bodyObject),
     headers: { 'Content-type': 'application/json; charset=UTF-8' },
   }).then((res) => res.json())
-    .then((msg) => { hol.innerText = msg.result; })
+    .then((msg) => {
+      hol.innerText = msg.result;
+      return setTimeout(() => {
+        hol.style.opacity = 0;
+      }, 3000);
+    })
     .catch((err) => {
-      if (err) hol.innerText = err;
+      if (err) {
+        hol.style.color = 'red';
+        hol.innerText = err;
+        return setTimeout(() => {
+          hol.style.opacity = 0;
+        }, 3000);
+      }
+      return err;
     });
 };
